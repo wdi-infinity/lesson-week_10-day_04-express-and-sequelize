eb2c080deff1756ffb9fcfe919a670e9dee94dc6
sajaalgadhi <saja.algadhi@outlook.com>
2019-03-20 13:35:00 +0300

    Add People list API endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { RSA_NO_PADDING } from 'constants';
+
 const app = express();
 const port = 3000;
 
@@ -12,4 +12,26 @@ app.get('/', (req, res) => {
 
 
 });
+const peopleList = [
+    { firstName: 'Saja', lastName: 'ALi' },
+    { firstName: 'Anfal', lastName: 'jafrai' },
+    { firstName: 'Ghadeer', lastName: 'ALkhathlan' },
+    { firstName: 'Me', lastName: 'Me' },
+    { firstName: 'Usman', lastName: 'Bashir' },
+
+
+];
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+
+        people: peopleList
+
+    });
+
+
+
+});
+//localhost:3000
+//localhost:3000/api/people
+
 app.listen(port, () => console.log(`express-api app listninig on port ${port}!`));
