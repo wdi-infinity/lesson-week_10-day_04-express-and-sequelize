5e47a1e1560eb89b1d69f766a978e85205fae47d
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-20 13:32:33 +0300

    api added

--- app.js
+++ app.js
@@ -9,4 +9,19 @@ app.get('/', (req, res) => {
     })
 })
 
+
+
+const people = [
+    { firstName: 'fajer', lastName: 'saleh' },
+    { firstName: 'fajer', lastName: 'saleh' },
+    { firstName: 'fajer', lastName: 'saleh' },
+    { firstName: 'fajer', lastName: 'saleh' }
+];
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        people: people
+    });
+})
+
 app.listen(port, () => console.log(`express-api listening on port ${port}!`))
