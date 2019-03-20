2929a7823545eb9e9c7dab3dc1a9c63b8de4d428
Majeed Almaimoni <MajeedM@Majeeds-MacBook-Pro.local>
2019-03-20 13:35:33 +0300

    Add people list api endpoint with dummy data

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express';
 const app = express();
 const port = 3000
+const people = [
+    { firstName: "Hala", lastName: "Almaimoni" },
+    { firstName: "hessa", lastName: "Alaqeel" },
+    { firstName: "sara", lastName: "alyahya" },
+    { firstName: "abdullah", lastName: "alfehaid" },
+]
+
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello wdi"
+    })
+})
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        people: people
     })
 })
 
