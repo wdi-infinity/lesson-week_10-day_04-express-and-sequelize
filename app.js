645172442d975368093dd8460cfeb03afb43c6a9
Mohammed <moh.jameel178@gmail.com>
2019-03-20 16:52:18 +0300

    Allow the user to Create a new person

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import models from './models';
+import bodyParser from 'body-parser';
+import { create } from 'domain';
 
 const app = express();
 const port = 3000;
 
+// MiddleWare
+app.use(bodyParser.json());
+
+// Routes
+
+// Root Path
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello WDI'
@@ -11,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 
-
+// Get All people
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
         .then(people => {
@@ -23,6 +31,7 @@ app.get('/api/people', (req, res) => {
 });
 
 
+// Get person by Record ID
 app.get('/api/person/:id', (req, res) => {
 
     if (!isNaN(req.params.id)) {
@@ -43,4 +52,20 @@ app.get('/api/person/:id', (req, res) => {
         res.status(406).json({ error: 'Invalid ID' })
     }
 })
+
+
+// create new person
+app.post('/api/person', (req, res) => {
+    models.Person.create(req.body)
+        .then(personNewFromDBB => {
+            res.status(201).json({ person: personNewFromDBB });
+        })
+        .catch(e => console.log(e));
+})
+
+//Update a person
+
+
+//Delete a person
+
 app.listen(port, () => console.log(`express-api app listeing on port ${port}!`))
