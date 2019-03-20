fcd12532a878f85118575ea6ad3cdfb7f552e963
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-20 11:48:37 +0300

    setup app index.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/',(req , res) => {
+res.status(200).json({
+    message: 'Hello WDI-Infinty!'
+})
+});
+app.listen(port, ()=> console.log(`express-api listening on port ${port}`));
