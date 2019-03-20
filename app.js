19a650f28e5a5c0ad7cad49d07ede905e36a4148
Abdullah <ab4ullahkh@gmail.com>
2019-03-20 15:33:44 +0300

    Add Route to show Person Details

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import models from './models';
+
 
 const app = express();
 const port = 3000;
@@ -11,19 +13,37 @@ app.get('/', (req,res) => {
 
 
 
-const people = [
-    {firstName: 'Abdullah', lastName:'Adel'},
-    {firstName: 'Ali', lastName:'Adel'},
-    {firstName: 'Micheal', lastName:'Fimann'}
-];
+// const people = [
+//     {firstName: 'Abdullah', lastName:'Adel'},
+//     {firstName: 'Ali', lastName:'Adel'},
+//     {firstName: 'Micheal', lastName:'Fimann'}
+// ];
 
 
 app.get('/api/people', (req,res) => {
-    res.status(200).json({
-    people: people
+    models.Person.findAll()
+    .then(people => {
+        res.status(200).json({ people: people });
+      })
+      .catch(e => console.log(e));
     });
-   });
-   
+
+
+
+    app.get('/api/person/:id', (req, res) => {
+         if( !isNaN(req.params.id)){
+                models.Person.findByPk(req.params.id).then(person => {
+                    if(person !== null){
+                    res.status(200).json({ person: person });
+                    } else {
+                        res.status(404).json({ error: 'Person Not Found' }); 
+                    }
+                })
+                .catch(e => console.log(e));
+    } else {
+        res.status(406).json({error: 'Invalid ID'})
+    }
+      });
 
 
 // localhost:3000
