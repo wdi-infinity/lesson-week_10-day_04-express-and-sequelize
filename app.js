f4e75ff1f113bbc28b22b3dae30ba95396de46e4
sara0alyahya <sara.a.alyahya@outlook.com>
2019-03-20 16:52:18 +0300

    Allow the user to create a new Person

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 import express from 'express';
 import models from './models';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
+/****** Middleware  ****/
+app.use(bodyParser.json());
+
+/**** Routes ***/
+
+// Root path
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello WDI-Infinity!"
     });
 })
-// Dummy data
-// const people = [
-//     {
-//         firstName: 'SARA', lastName: 'ALYAHYA'
-//     },
-//     {
-//         firstName: 'Hessa', lastName: 'ALAQIL'
-//     },
-//     {
-//         firstName: 'Fajer', lastName: 'ALBAKIRI'
-//     }
-// ];
+
 // localhost:3000
 // localhost:3000/
+
+// get all people
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
         .then(peoplefromDB => {
@@ -35,6 +33,8 @@ app.get('/api/people', (req, res) => {
 
 
 // http://localhost:3000/api/person/2
+
+// get Person by record ID
 app.get('/api/person/:id', (req, res) => {
     if (!isNaN(req.params.id)) {
         models.Person.findByPk(req.params.id)
@@ -56,5 +56,19 @@ app.get('/api/person/:id', (req, res) => {
     }
 
 });
+
+
+// POST  http://localhost:3000/api/person
+
+// create new person
+app.post('/api/person', (req, res) => {
+    models.Person.create(req.body)
+        .then(personNewFromDB => {
+            res.status(201).json({ person: personNewFromDB });
+        })
+        .catch(e => console.log(e));
+})
+
+
 
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
