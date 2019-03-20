88f32b03f369a604cac098e394fc4e21fba32ebe
rawanah995 <rawanah995@gmail.com>
2019-03-20 11:48:48 +0300

    commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const app = express();
-
+const port = 3000;
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello wdi"
     })
 })
+app.listen(3000, () => console.log(`express-api listening on port ${port}!`))
