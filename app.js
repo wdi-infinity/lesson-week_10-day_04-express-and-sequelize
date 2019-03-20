06d6699ee8973a4a98f942cd51578312303eb7f4
hessaAqeel <hessa1aqeel@gmail.com>
2019-03-20 16:52:17 +0300

    Allow the User to Create a new Person

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import models from './models';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
-//  routre
+
+// Middleware 
+app.use(bodyParser.json());
+
+
+//  routes
+
+// Root Path
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello ppl !"
@@ -18,14 +26,7 @@ app.get('/', (req, res) => {
 //     });
 // });
 
-// const people = [
-//     { firstName: 'Hes', lasName: 'Aqeel' },
-//     { firstName: 'Hal', lasName: 'Maimoni' },
-//     { firstName: 'Sara', lasName: 'Yahya' },
-//     { firstName: 'Rawan', lasName: 'Ahmadi' }
-
-// ];
-
+// Get All People 
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
         .then(people => {
@@ -35,10 +36,7 @@ app.get('/api/people', (req, res) => {
         })
         .catch(e => console.log(e))
 });
-app.listen(port, () => console.log(`express-api listening on port ${port}`))
-
-
-
+// Gets Person By Record ID
 app.get('/api/person/:id', (req, res) => {
 
     //    To test 
@@ -46,6 +44,7 @@ app.get('/api/person/:id', (req, res) => {
     // To test if id is working 
     // res.status(200).json({ user_id: req.params.id })
     // We need only Numbers as an IDs ! so we added a condition 
+
     if (!isNaN(req.params.id)) {
         models.Person.findByPk(req.params.id)
             .then(person => {
@@ -63,66 +62,21 @@ app.get('/api/person/:id', (req, res) => {
 });
 
 
+// Create new person 
+app.post('/api/person', (req, res) => {
+    // add data to db 
+    models.Person.create(req.body)
+        .then(personNewFromDB => {
+            res.status(201).json(({ person: personNewFromDB }))
+        })
+        .catch(e => console.log(e));
+});
+
+// localhost:3000/people.html 
+//  localhost:3000/api/people
+// 
 
 
 
+app.listen(port, () => console.log(`express-api listening on port ${port}`))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Usman's code****
-
-// import express from 'express';
-// import models from './models';
-
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: 'Hello WDI-Infinity!'
-//     });
-// });
-
-// // const peopleList = [
-// //   { firstName: 'Usman', lastName: 'Bashir' },
-// //   { firstName: 'Michael', lastName: 'Finneran' },
-// //   { firstName: 'Ghadeer', lastName: 'Alkhathlan' },
-// //   { firstName: 'Saja', lastName: 'Algadhi' },
-// //   { firstName: 'Hussah', lastName: 'Alakeel' },
-// //   { firstName: 'Ahmad', lastName: 'Alqahtani' },
-// //   { firstName: 'Abdullah', lastName: 'Alfehaid' }
-// // ];
-
-// app.get('/api/people', (req, res) => {
-//     models.Person.findAll()
-//         .then(peopleFromDB => {
-//             res.status(200).json({
-//                 people: peopleFromDB
-//             });
-//         })
-//         .catch(e => console.log(e));
-// });
-
-// // localhost:3000/people.html 
-// // localhost:3000/api/people
-
-// app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
