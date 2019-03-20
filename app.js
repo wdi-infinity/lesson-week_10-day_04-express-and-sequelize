31b03db8c34e34494ea39a9637acbe7f548b47e8
loltisha <alyahadiem@gmail.com>
2019-03-20 15:33:37 +0300

    add route to show person details

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import models from './models';
 const app = express();
 const port=3000;
 
@@ -10,20 +10,41 @@ app.get('/', (req, res) => {
   });
 });
 
-const people = [
-  { firstName: 'alia', lastName: 'alrashidi' },
-  { firstName: 'usman', lastName: 'bashir' },
-  { firstName: 'ghdeer', lastName: 'alkhathlan' },
-  { firstName: 'michael', lastName: 'finneran' },
-  { firstName: 'fajer', lastName: 'albakiri' },
-];
+// const people = [
+//   { firstName: 'alia', lastName: 'alrashidi' },
+//   { firstName: 'usman', lastName: 'bashir' },
+//   { firstName: 'ghdeer', lastName: 'alkhathlan' },
+//   { firstName: 'michael', lastName: 'finneran' },
+//   { firstName: 'fajer', lastName: 'albakiri' },
+// ];
 
-
+app.get('/api/person/:id', (req, res) => {
+  // res.status(200).json({user_id:'Working...'});
+  if(!isNaN(req.params.id)){
+    models.Person.findByPk(req.params.id)
+    .then(person => {
+      if (person!==null)
+      {  res.status(200).json({person: person});
+      }else{
+        res.status(404).json({error: 'person not found'});
+      }
+    })
+    .catch(e => console.log(e)); 
+  }else{
+    res.status(406).json({error: "invalid ID"});
+  }
+  
+  });
+  
 // localhost: 3000/api/people
 app.get('/api/people', (req, res) => {
-  res.status(200).json({
-    people: people,
-  });
+  models.Person.findAll()
+  .then(people => {
+    res.status(200).json({
+      people: people,
+    });
+  })
+  .catch(e => console.log(e)); 
 });
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
