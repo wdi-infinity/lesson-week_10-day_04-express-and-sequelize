f2aa2644023bff9f3277a26998ef7c463da6449e
rawanah995 <rawanah995@gmail.com>
2019-03-20 13:35:36 +0300

    add people list API endpoint with Dummy data

--- app.js
+++ app.js
@@ -5,5 +5,23 @@ app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello wdi"
     })
-})
+});
+
+const people = [
+    { firstName: 'Rawan', lastName: 'Alahmmadi' },
+    { firstName: 'Rawan1', lastName: 'Alahmmadi' },
+    { firstName: 'Rawan2', lastName: 'Alahmmadi' },
+    { firstName: 'Rawan3', lastName: 'Alahmmadi' },
+    { firstName: 'Rawan4', lastName: 'Alahmmadi' },
+    { firstName: 'Rawan5', lastName: 'Alahmmadi' }
+]
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        people: people
+    });
+});
+
+
+
 app.listen(3000, () => console.log(`express-api listening on port ${port}!`))
