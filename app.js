7578bf78b518f15ac555dab544bbe543abe18159
Mhammad1704 <natgeo1416@hotmail.com>
2019-03-20 15:31:54 +0300

    add route to show person details

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import models from './models';
 
 const app =  express();
 const port = 3000;
@@ -9,23 +10,47 @@ res.status(200).json({
 });
 });
 
-const peopleList = [
-    { firstName: 'mohmamag' ,lastName:'omar'},
-    { firstName: 'ali' ,lastName:'narul'},
-    { firstName: 'ganduf' ,lastName:'omar'},
-    { firstName: 'mohmamag' ,lastName:'omar'},
-    { firstName: 'ali' ,lastName:'narul'},
-    { firstName: 'ganduf' ,lastName:'omar'},
-    { firstName: 'mohmamag' ,lastName:'omar'},
-    { firstName: 'ali' ,lastName:'narul'},
-    { firstName: 'ganduf' ,lastName:'omar'}
-]
+// const peopleList = [
+//     { firstName: 'mohmamag' ,lastName:'omar'},
+//     { firstName: 'ali' ,lastName:'narul'},
+//     { firstName: 'ganduf' ,lastName:'omar'},
+//     { firstName: 'mohmamag' ,lastName:'omar'},
+//     { firstName: 'ali' ,lastName:'narul'},
+//     { firstName: 'ganduf' ,lastName:'omar'},
+//     { firstName: 'mohmamag' ,lastName:'omar'},
+//     { firstName: 'ali' ,lastName:'narul'},
+//     { firstName: 'ganduf' ,lastName:'omar'}
+// ]
 
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: peopleList
+    models.Person.findAll()
+    .then(peopleFromDB => {
+res.status(200).json({
+        people: peopleFromDB
        } );
+    })
+    .catch(e => console.log(e))
+    
 });
+
+app.get('/api/person/:id', (req, res) => {
+   if(!isNaN(req.params.id)){
+ models.Person.findByPk(req.params.id)
+    
+      .then(person => {
+        if(person !== null ){
+            res.status(200).json({person: person});
+        } else {
+            res.status(416).json({message: "person not found"});
+        
+        }
+      })
+      .catch(e => console.log(e));
+  
+} else {
+    res.status(406).json({error: 'onvilde ID'});
+}
+})
 
 app.listen(port, () => {
     console.log(`.:|:|:|:|:|express-api app Server started on ${port}|:|:|:|:.`);
