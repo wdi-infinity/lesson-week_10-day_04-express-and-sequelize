c7a3a30aae2ad69a441cfa8bf205f8cde469afbe
Mohammed <4m.4s.2013@gmail.com>
2019-03-20 13:33:28 +0300

    api people

--- app.js
+++ app.js
@@ -9,5 +9,21 @@ app.get('/',(req, res) => {
     });
 });
 
+const peopleList = [
+    { firstName: 'Mohammmed', lastName: 'Rashidi' },
+    { firstName: 'Michael', lastName: 'Finneran' },
+    { firstName: 'Ghadeer', lastName: 'ALkhathlan' },
+    { firstName: 'Saja', lastName: 'Algadhi' },
+    { firstName: 'Ahmed', lastName: 'Alqahtani' },
+    {firstName:'Hessa', lastName:'ALaqeel'},
+    { firstName: 'Abdullah', lastName: 'Rashidi' },
+
+]
+
+app.get('/api/people',(req, res)=>{
+    res.status(200).json({
+        people: peopleList
+    })
+})
 
 app.listen(3000, () => console.log(`express-api app listing on port ${port}!`));
