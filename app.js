08bbd50ed9df771b51d5f540d6632a5356de4f6d
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-03-20 13:35:36 +0300

    add people list api endpoint with dummy data

--- app.js
+++ app.js
@@ -9,4 +9,28 @@ message: "Hello WDI-Infinity!"
 });
 
 
+
+const people = [
+{firstName:'Waad',lastName:'Baabdullah'},
+{firstName:'Asma',lastName:'Baabdullah'},
+{firstName:'Afrah',lastName:'Baabdullah'},
+{firstName:'Samah',lastName:'Baabdullah'},
+{firstName:'Tahani',lastName:'Baabdullah'},
+{firstName:'khalid',lastName:'Baabdullah'},
+{firstName:'Mohammad',lastName:'Baabdullah'}
+];
+
+
+//new route
+//localhost:3000
+//localhost:3000/api/people api is a convention
+//each apis must come after localhost:3000/api/ is the best way  
+app.get('/api/people',(req,res)=>{
+res.status(200).json({
+people:people
+});
+
+});
+
+
 app.listen(port,()=>console.log(`express-api app listening on port ${port}!`));
