1e1511cab2ba0a12586d06940b7bf5d1f804a410
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-20 15:33:38 +0300

    Add route to show person details

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import models from './models'
 
 const app = express();
 const port = 3000;
@@ -8,16 +9,45 @@ res.status(200).json({
     message: 'Hello WDI-Infinty!'
 })
 });
-const people = [
-    {first_name: 'abdulmohsin' , last_name: 'sharhan'},
-    {first_name: 'Ahmed' , last_name: 'alqhtani'},
-    {first_name: 'Saud' , last_name: 'almutari'},
-    {first_name: 'Moath' , last_name: 'althwid'}
-]
-//localhost:3000/api/people
+// const people = [
+//     {first_name: 'abdulmohsin' , last_name: 'sharhan'},
+//     {first_name: 'Ahmed' , last_name: 'alqhtani'},
+//     {first_name: 'Saud' , last_name: 'almutari'},
+//     {first_name: 'Moath' , last_name: 'althwid'}
+// ]
+//localhost:3000/api/people display all list
 app.get('/api/people' , (req , res ) => {
-    res.status(200).json({
-      people: people
+    //Find all people
+    models.Person.findAll()
+    .then(people => {
+        res.status(200).json({
+            //display array of people
+            people: people
+          })
     })
+    .catch( e => console.log(e))
+  
+})
+
+//localhost:3000/api/person/:id to Find one person at a time
+app.get('/api/person/:id' , (req , res ) => {
+
+    const id = req.params.id
+    if(!isNaN(id)){
+    models.Person.findByPk(id)//One person
+
+    .then(person => {
+        if (person !== null){
+        res.status(200).json({  person: person  })
+        }
+        else {
+            res.status(404).json({  error: 'Person Not Found'  })
+        }
+    })
+    .catch( e => console.log(e))
+}
+else {
+    res.status(406).json({  error: 'Invalid ID'  })
+}
 })
 app.listen(port, ()=> console.log(`express-api listening on port ${port}`));
