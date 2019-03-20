a533214ce6a287b63b5ea0165b96833ef64feb11
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-20 15:33:16 +0300

    create routes to show person details

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 import express from 'express';
+import models from './models';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Hello wdi"
-    })
-})
+app.get('/api/people', (req, res) => {
 
+    models.Person.findAll()
+        .then(peopleFromDB => {
+            res.status(200).json({
+                people: peopleFromDB
+            });
+        })
+        .catch(e => console.log(e));
+});
 
+app.get('/api/person/:id', (req, res) => {
+
+    if (!isNaN(req.params.id)) {
+        models.Person.findByPk(req.params.id)
+            .then(personFromDB => {
+
+                if (personFromDB !== null) {
+                    res.status(200).json({ person: personFromDB });
+                } else {
+                    res.status(404).json({ message: "person NOT found" });
+                }
+
+            })
+            .catch(e => console.log(e));
+    } else {
+        res.status(406).json({ error: "Invalid ID" });
+    }
+
+});
 
 const people = [
     { firstName: 'fajer', lastName: 'saleh' },
