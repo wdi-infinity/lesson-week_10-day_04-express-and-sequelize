4c19684f20e7f0978e749cd52e8f90887fffd477
Mohammed <moh.jameel178@gmail.com>
2019-03-20 15:33:29 +0300

    Add Route to show Person Details

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import models from './models';
 
 const app = express();
 const port = 3000;
@@ -12,16 +13,34 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: peopleList
-    });
+    models.Person.findAll()
+        .then(people => {
+            res.status(200).json({
+                people: peopleFromDB
+            });
+        })
+        .catch(e => console.log(e))
 });
 
-const peopleList = [
-    { firstName: 'Mohammed', lastName: 'Jameel' },
-    { firstName: 'Ahmed', lastName: 'Salaman' },
-    { firstName: 'Waleed', lastName: 'Ahmed' },
-    { firstName: 'Salem', lastName: 'Shehri' }
-];
 
+app.get('/api/person/:id', (req, res) => {
+
+    if (!isNaN(req.params.id)) {
+        models.Person.findByPk(req.params.id)
+            .then(person => {
+                if (person !== null) {
+                    res.status(200).json({ person: person });
+                }
+                else {
+                    res.status(404).json({
+                        error: 'person not found'
+                    });
+                }
+            })
+            .catch(e => console.log(e))
+    }
+    else {
+        res.status(406).json({ error: 'Invalid ID' })
+    }
+})
 app.listen(port, () => console.log(`express-api app listeing on port ${port}!`))
