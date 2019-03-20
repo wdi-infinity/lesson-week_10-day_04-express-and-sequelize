d07631f10bcc4e8258cbb9c577bdb1d999b3d3f7
hessaAqeel <hessa1aqeel@gmail.com>
2019-03-20 11:49:28 +0300

    Add Root Route

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello ppl !"
+    });
+});
+
+app.listen(port, () => console.log(`express-api listening on port ${port}`))
