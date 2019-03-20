08a76426840da218fcb1b560fe69aa068e93464e
sara saleh alofi <saraalawfi1995@gmail.com>
2019-03-20 15:33:38 +0300

    Add Route to show Person Details

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
 import express from 'express';
+import models from './models';
 
 const app = express();
-
 const port = 3000;
+
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Hello from WDI 3'
+        message: 'Hello WDI-Infinity!'
     });
 });
-const people = [
-    {
-        firstName: 'Sara', lastName: 'Alawfi'
-    },
-    {
-        firstName: 'Moroj', lastName: 'Alawfi'
-    },
-    {
-        firstName: 'Alaa', lastName: 'Alawfi'
-    },
-];
+
+// const peopleList = [
+//   { firstName: 'Usman', lastName: 'Bashir' },
+//   { firstName: 'Michael', lastName: 'Finneran' },
+//   { firstName: 'Ghadeer', lastName: 'Alkhathlan' },
+//   { firstName: 'Saja', lastName: 'Algadhi' },
+//   { firstName: 'Hussah', lastName: 'Alakeel' },
+//   { firstName: 'Ahmad', lastName: 'Alqahtani' },
+//   { firstName: 'Abdullah', lastName: 'Alfehaid' }
+// ];
+
+
+// const people = [
+//     {
+//         firstName: 'Sara', lastName: 'Alawfi'
+//     },
+//     {
+//         firstName: 'Moroj', lastName: 'Alawfi'
+//     },
+//     {
+//         firstName: 'Alaa', lastName: 'Alawfi'
+//     },
+// ];
 
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: people
-    });
-
+    models.Person.findAll()
+        .then(peopleFromDB => {
+            res.status(200).json({
+                people: peopleFromDB
+            });
+        })
+        .catch(e => console.log(e));
 });
 
-app.listen(port, () => console.log(`express-api app listen to port 3000 ${port}`))
+// find one person by id
+app.get('/api/person/:id', (req, res) => {
+    if (!isNaN(req.params.id)) {
+        models.Person.findByPk(req.params.id)
+            .then(person => {
+                if (person !== null) {
+                    res.status(200).json({ person: person });
+                } else {
+                    res.status(404).json({ error: 'Person Not Found' });
+                }
+            })
+            .catch(e => console.log(e));
+    } else {
+        res.status(406).json({ error: 'Invalid ID' });
+    }
+});
+
+// localhost:3000/people.html 
+// localhost:3000/api/people
+
+app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
+
+
