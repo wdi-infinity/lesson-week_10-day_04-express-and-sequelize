2cfece73bc466381cbd51415b114bbda821009fb
saud9030 <saud.amakn@gmail.com>
2019-03-20 13:33:12 +0300

    API route

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ app.get('/', (req,res) => {
         message: "Hello WDI-infinity"
     });
 });
+const people = [
+    {firstName: 'saud', lastName: 'Almutairi'},
+    {firstName: 'nors', lastName: 'Abdullah'},
+    {firstName: 'mohammed', lastName: 'Saja'},
+]
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        //the first people is just the key, so it's changable but not the second one.
+        people: people
+    });
+});
+
 
 app.listen(port, ()=> console.log(`working on port ${port}`));
