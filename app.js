fb638c0b131b808f254594c7827be9789345d126
ceahmad <almansour.a.ahmad@gmail.com>
2019-03-20 15:32:20 +0300

    Add Route to Show Person Details

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import models from './models';
 const app = express();
 const port = 3000;
 
@@ -10,20 +10,48 @@ res.status(200).json({
 });
 
 
-const people = [
-    { firstName: 'Ahmad', lastName:'Almansor' },
-    { firstName: 'Abdullah', lastName:'Ahmad' },
-    { firstName: 'Abdullah', lastName:'Alfehaid' },
-    { firstName: 'Ahmad', lastName:'Almansor' },
-    { firstName: 'Ahmad', lastName:'Almansor' }    
-];
+// const people = [
+//     { firstName: 'Ahmad', lastName:'Almansor' },
+//     { firstName: 'Abdullah', lastName:'Ahmad' },
+//     { firstName: 'Abdullah', lastName:'Alfehaid' },
+//     { firstName: 'Ahmad', lastName:'Almansor' },
+//     { firstName: 'Ahmad', lastName:'Almansor' }    
+// ];
 
 
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: people
-    });
+    models.Person.findAll()
+    .then(people => {
+        res.status(200).json({
+            people: people  // people from DB the 2nd one the 1nd is key of object 
+        });
+    })
+        .catch(e => console.log(e)); 
 });
+
+
+
+app.get('/api/person/:id', (req, res) => {
+    if( !isNaN(req.params.id) )
+    models.Person.findByPk(req.params.id)
+    .then(person => {
+        if(person !== null ){
+            res.status(200).json({person: person})
+        } else {
+
+            res.status(406).json({ error : 'Person Not Found'})          
+        } 
+    })
+    .catch(e => console.log(e)); 
+else {
+    res.status(200).json({ erroe: 'Invalid ID' })
+}
+});
+
+
+
+
+
 // localhost:3000/
 // localhost:3000/api/people
 
