707728598666e8041fae5aa7dea7e120ed37acee
asmaquraishi <asmaq66@gmail.com>
2019-03-20 13:35:30 +0300

    added list of people api endpoint with dummy data

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.get('/', (req ,res) => {
     })
 })
 
+const peopleList =[
+    {firstName: 'Asma', lastName: 'Quraishi'},
+    {firstName: 'sara', lastName: 'ahmad'},
+    {firstName: 'badriah', lastName: 'shehri'},
+    {firstName: 'usman', lastName: 'bashir'},
+    {firstName: 'reem', lastName: 'fahad'}
+];
+
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({
+        people: peopleList
+    });
+});
 
 const PORT = 3000;
 
