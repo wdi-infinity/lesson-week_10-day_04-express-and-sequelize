27e7d73fc971cdd45ba5450c03d1c473ea8891e4
hessaAqeel <hessa1aqeel@gmail.com>
2019-03-20 13:34:40 +0300

    Add people list API endpoint

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+//  routre
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello ppl !"
     });
 });
 
+// new routre
+// app.get('/api/people', (req, res) => {
+//     res.status(200).json({
+//         message: "API/PEOPLE Works!"
+//     });
+// });
+
+const people = [
+    { firstName: 'Hes', lasName: 'Aqeel' },
+    { firstName: 'Hal', lasName: 'Maimoni' },
+    { firstName: 'Sara', lasName: 'Yahya' },
+    { firstName: 'Rawan', lasName: 'Ahmadi' }
+
+];
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        people: people
+    });
+});
 app.listen(port, () => console.log(`express-api listening on port ${port}`))
