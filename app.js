c9a0f0b0649876d3c1e84e6a6334d4c656653a56
Salem Alshehri <sal5m@hotmail.com>
2019-03-20 11:49:37 +0300

    Add Root Rpute

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello"
+    });
+});
+
+app.listen(port, () => console.log(`express-api and listening on port ${port}!`));
