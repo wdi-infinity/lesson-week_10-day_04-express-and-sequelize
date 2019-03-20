b5ffa95b60444c8aaaea251fa603ad6689281430
Mhammad1704 <natgeo1416@hotmail.com>
2019-03-20 11:49:30 +0300

    add root route

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app =  express();
+const port = 3000;
+
+app.get('/',(req,res)=> {
+res.status(200).json({
+    message:'Hello WDI-Infinity!'
+});
+});
+
+
+app.listen(port, () => {
+    console.log(`.:|:|:|:|:|express-api app Server started on ${port}|:|:|:|:.`);
+});
