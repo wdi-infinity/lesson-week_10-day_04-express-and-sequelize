ba766f6076d06dccf94a8da6643fa0ddb8b7e35f
sara saleh alofi <saraalawfi1995@gmail.com>
2019-03-20 16:52:18 +0300

    Allow the user to Create a new Person

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
 import express from 'express';
 import models from './models';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
+/*** Middleware ***/
+app.use(bodyParser.json());
+
+
+/*** Routse ***/
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello WDI-Infinity!'
     });
 });
 
-// const peopleList = [
-//   { firstName: 'Usman', lastName: 'Bashir' },
-//   { firstName: 'Michael', lastName: 'Finneran' },
-//   { firstName: 'Ghadeer', lastName: 'Alkhathlan' },
-//   { firstName: 'Saja', lastName: 'Algadhi' },
-//   { firstName: 'Hussah', lastName: 'Alakeel' },
-//   { firstName: 'Ahmad', lastName: 'Alqahtani' },
-//   { firstName: 'Abdullah', lastName: 'Alfehaid' }
-// ];
-
-
-// const people = [
-//     {
-//         firstName: 'Sara', lastName: 'Alawfi'
-//     },
-//     {
-//         firstName: 'Moroj', lastName: 'Alawfi'
-//     },
-//     {
-//         firstName: 'Alaa', lastName: 'Alawfi'
-//     },
-// ];
-
+// get all people
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
         .then(peopleFromDB => {
@@ -42,11 +26,11 @@ app.get('/api/people', (req, res) => {
         })
         .catch(e => console.log(e));
 });
-
+// Get person by record Id
 // find one person by id
 app.get('/api/person/:id', (req, res) => {
     if (!isNaN(req.params.id)) {
-        models.Person.findByPk(req.params.id)
+        models.Person.findByPk(req.body)
             .then(person => {
                 if (person !== null) {
                     res.status(200).json({ person: person });
@@ -60,8 +44,21 @@ app.get('/api/person/:id', (req, res) => {
     }
 });
 
+
+//Create new person
+app.post('/api/person', (req, res) => {
+    models.Person.create(req.body)
+        .then(personNewFromDb => {
+            res.status(201).json({ person: personNewFromDb });
+        })
+        .catch(e => console.log(e));
+
+});
+
 // localhost:3000/people.html 
 // localhost:3000/api/people
+// localhost:3000/api/person/2
+// POST localhost:3000/api/person/2
 
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
 
