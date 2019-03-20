a46078af29ebe9f1551be47efb90377fefe053e7
loltisha <alyahadiem@gmail.com>
2019-03-20 13:32:43 +0300

    Add list of people in array

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import express from 'express';
 
 const app = express();
+const port=3000;
 
-app.get('/', (req,res) => {
+// localhost: 3000/
+app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello WDI-Infinity!',
   });
 });
 
-app.listen(3000, () => console.log('express-api app listening on port 3000!'));
+const people = [
+  { firstName: 'alia', lastName: 'alrashidi' },
+  { firstName: 'usman', lastName: 'bashir' },
+  { firstName: 'ghdeer', lastName: 'alkhathlan' },
+  { firstName: 'michael', lastName: 'finneran' },
+  { firstName: 'fajer', lastName: 'albakiri' },
+];
+
+
+// localhost: 3000/api/people
+app.get('/api/people', (req, res) => {
+  res.status(200).json({
+    people: people,
+  });
+});
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
