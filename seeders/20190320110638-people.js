4a635da2c61a226649bd445b613fc84b3405770d
Nahed_2019 <n.add55@hotmail.com>
2019-03-20 14:33:34 +0300

    Add People Sequelize model with seed data

---
+++ seeders/20190320110638-people.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+
+    return queryInterface.bulkInsert('people', [
+      { first_name: 'Nawaf', last_name: 'Moahmmed', created_at: new Date, updated_at: new Date },
+      { first_name: 'jaser', last_name: 'yonis', created_at: new Date, updated_at: new Date },
+      { first_name: 'Aser', last_name: 'yonis', created_at: new Date, updated_at: new Date },
+      { first_name: 'Aser', last_name: 'yonis', created_at: new Date, updated_at: new Date }
+    ], {});
+
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('people', null, {});
+    */
+  }
+};
