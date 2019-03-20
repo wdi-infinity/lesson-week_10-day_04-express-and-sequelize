7d37a32f0563f3616f95d69c44d8627124b18800
Mohammed <4m.4s.2013@gmail.com>
2019-03-20 15:34:46 +0300

    Add Route to show Person Details

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 import express from 'express';
+import models from './models';
 
 const app = express();
 const port = 3000;
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({
-        message:'Hello WDI-Unfinity!'
+        message: 'Hello WDI-Infinity!'
     });
 });
 
-const peopleList = [
-    { firstName: 'Mohammmed', lastName: 'Rashidi' },
-    { firstName: 'Michael', lastName: 'Finneran' },
-    { firstName: 'Ghadeer', lastName: 'ALkhathlan' },
-    { firstName: 'Saja', lastName: 'Algadhi' },
-    { firstName: 'Ahmed', lastName: 'Alqahtani' },
-    {firstName:'Hessa', lastName:'ALaqeel'},
-    { firstName: 'Abdullah', lastName: 'Rashidi' },
+// const peopleList = [
+//   { firstName: 'Usman', lastName: 'Bashir' },
+//   { firstName: 'Michael', lastName: 'Finneran' },
+//   { firstName: 'Ghadeer', lastName: 'Alkhathlan' },
+//   { firstName: 'Saja', lastName: 'Algadhi' },
+//   { firstName: 'Hussah', lastName: 'Alakeel' },
+//   { firstName: 'Ahmad', lastName: 'Alqahtani' },
+//   { firstName: 'Abdullah', lastName: 'Alfehaid' }
+// ];
 
-]
+app.get('/api/people', (req, res) => {
+    models.Person.findAll()
+        .then(peopleFromDB => {
+            res.status(200).json({
+                people: peopleFromDB
+            });
+        })
+        .catch(e => console.log(e));
+});
 
-app.get('/api/people',(req, res)=>{
-    res.status(200).json({
-        people: peopleList
-    })
-})
+app.get('/api/person/:id', (req, res) => {
+    if (!isNaN(req.params.id)) {
 
-app.listen(3000, () => console.log(`express-api app listing on port ${port}!`));
+        models.Person.findByPk(req.params.id)
+            .then(person => {
+                if (person !== null) {
+                    res.status(200).json({ person: person });
+                } else {
+                    res.status(404).json({ error: 'Person Not Found' });
+                }
+            })
+      
+      // 2==>>> res.status(200).json({person: person});
+        
+    .catch(e=> console.log(e));
+        }else{
+            res.status(406).json({error: "Invalid ID"});
+
+        }
+    // 1==>>> res.status(200).json({user_id: req.params.id});
+});
+
+
+app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
