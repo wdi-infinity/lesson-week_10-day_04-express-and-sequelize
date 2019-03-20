d679dde81f8aee35c03791b22462834c177ec263
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-03-20 13:36:08 +0300

    Add People List API endpoint with Dummy Data

--- app.js
+++ app.js
@@ -9,4 +9,20 @@ app.get('/', (req, res) => {
     });
 });
 
+const peopleList = [
+  { firstName: 'Ahmad', lastName: 'Yaseen'},
+  { firstName: 'Abdulrahman', lastName: 'Alibrahim'},
+  { firstName: 'Usman', lastName: 'Bashir'},
+  { firstName: 'Micheal', lastName: 'Finneran'},
+  { firstName: 'Ghadeer', lastName: 'Alkhathlan'},
+  { firstName: 'Saja', lastName: 'Alghadi'},
+  { firstName: 'Ahmad', lastName: 'Alqahtani'}
+];
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        people: peopleList
+    });
+});
+
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
