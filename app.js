00a654d1c3e754ed0b634d2f6e9e6c363f862a7e
abduwibr <abduwibr@gmail.com>
2019-03-20 16:51:26 +0300

    new person creation feature enabled

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import models from './models';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.status(200).json({
@@ -9,17 +13,44 @@ app.get('/', (req,res) => {
     });
 });
 
-const peopleList = [
-    {firstName: 'Abdulrahman', lastName: 'Alibrahim'},
-    {firstName: 'Usman', lastName: 'Bashir'},
-    {firstName: 'Michael', lastName: 'Finneran'},
-    {firstName: 'Ghadeer', lastName: 'AlKhathlan'},
-];
-
+// Get person by record ID
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: peopleList;
-    });
+
+    models.Person.findAll()
+    .then(peopleList => {
+        res.status(200).json({
+            people: peopleList
+        });        
+    })
+    
+    .catch(e => console.log(e));
+});
+
+app.get('/api/person/:id', (req,res) => {
+    if( !isNaN(req.params.id)) {
+    models.Person.findByPk(req.params.id)
+    .then(person => {
+        
+        if (person !== null){
+            res.status(200).json({ person: person });
+        } else { 
+            res.status(404).json({ error: 'Person Not Found'});
+        }
+    })
+    .catch(e => console.log(e));
+    } else {
+    res.status(406).json({ error: "Invalid ID" })
+    }
+});
+
+app.post('/api/person', (req, res) => {
+    models.Person.create(req.body)
+    .then(person => {
+        res.status(200).json({ result: req.body});
+    })
+    .catch(e => console.log(e));
 });
 
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`))
+
+
