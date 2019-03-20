b222db83e6918995414bdfa18571bca81e173275
Majeed Almaimoni <MajeedM@Majeeds-MacBook-Pro.local>
2019-03-20 16:51:54 +0300

    Allow the user to create a new person

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import models from './models';
+import bodyParser from 'body-parser'
 
 const app = express();
 const port = 3000
-// const people = [
-//     { firstName: "Hala", lastName: "Almaimoni" },
-//     { firstName: "hessa", lastName: "Alaqeel" },
-//     { firstName: "sara", lastName: "alyahya" },
-//     { firstName: "abdullah", lastName: "alfehaid" },
-// ]
 
+// Middleware
+app.use(bodyParser.json())
+
+/***  Routes ***/
+
+// Route path
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Hello wdi"
     })
 })
+// Get All people
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
     .then(people => {
@@ -22,9 +24,10 @@ app.get('/api/people', (req, res) => {
             people: people
         })
     })
-    .catch(e => console.log(e))
-   
+    .catch(e => console.log(e))  
 })
+
+// Get person by record id
 app.get('/api/person/:id', (req, res) => {
     if( !isNaN(req.params.id)){
     models.Person.findByPk(req.params.id)
@@ -34,14 +37,21 @@ app.get('/api/person/:id', (req, res) => {
     } else {
         res.status(404).json({ error: "person not found"})
     }
-}
-)
-    
-    .catch(e => console.log(e))
-        
+})
+    .catch(e => console.log(e))      
 } else {
     res.status(418).json({error: 'Invalid ID'})
 }
+})
+
+// Create new person
+app.post("/api/person", (req, res) => {
+    const person = req.body
+    models.Person.create(person)
+    .then(persondb => {
+        res.status(200).json({ person: persondb })
+    })
+    .catch(e => console.log(e))
 })
 
 app.listen(port, () => console.log(`express-api listening on port ${port}!`))
