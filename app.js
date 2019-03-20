4636d9ad4eb3039d24653426a32c27e06920db86
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-03-20 15:33:44 +0300

    Add Route to show Person Details

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import models from './models'
 
 const app = express();
 const port = 3000;
@@ -9,20 +10,60 @@ app.get('/', (req, res) => {
     });
 });
 
-const peopleList = [
-  { firstName: 'Ahmad', lastName: 'Yaseen'},
-  { firstName: 'Abdulrahman', lastName: 'Alibrahim'},
-  { firstName: 'Usman', lastName: 'Bashir'},
-  { firstName: 'Micheal', lastName: 'Finneran'},
-  { firstName: 'Ghadeer', lastName: 'Alkhathlan'},
-  { firstName: 'Saja', lastName: 'Alghadi'},
-  { firstName: 'Ahmad', lastName: 'Alqahtani'}
-];
+// const peopleList = [{
+//         firstName: 'Ahmad',
+//         lastName: 'Yaseen'
+//     },
+//     {
+//         firstName: 'Abdulrahman',
+//         lastName: 'Alibrahim'
+//     },
+//     {
+//         firstName: 'Usman',
+//         lastName: 'Bashir'
+//     },
+//     {
+//         firstName: 'Micheal',
+//         lastName: 'Finneran'
+//     },
+//     {
+//         firstName: 'Ghadeer',
+//         lastName: 'Alkhathlan'
+//     },
+//     {
+//         firstName: 'Saja',
+//         lastName: 'Alghadi'
+//     },
+//     {
+//         firstName: 'Ahmad',
+//         lastName: 'Alqahtani'
+//     }
+// ];
 
 app.get('/api/people', (req, res) => {
-    res.status(200).json({
-        people: peopleList
-    });
+    models.Person.findAll()
+        .then(peopleFromDB => {
+            res.status(200).json({
+                people: peopleFromDB
+            });
+        })
+        .catch(e => console.log(e));
+});
+
+app.get('/api/person/:id', (req, res) => {
+    if( !isNaN(req.params.id) ) {
+        models.Person.findByPk(req.params.id)
+            .then(person => {
+                if (person !== null) {
+                    res.status(200).json({ person: person });
+                } else {
+                    res.status(404).json({ error: 'Person Not Found'});
+                }
+            })
+            .catch(e => console.log(e));
+    } else {
+        res.status(406).json({ error: 'Invalid ID'});
+    }
 });
 
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
