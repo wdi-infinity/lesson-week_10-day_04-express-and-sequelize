424b846c76fb33d0c1a2c59bf5dbbd9d33750a35
Nahed_2019 <n.add55@hotmail.com>
2019-03-21 13:57:51 +0300

    add route to show article deatils

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.delete('/api/person/:id', (req, res) => {
         })
         .catch(e => console.log(e));
 });
+
 // eslint-disable-next-line no-console
 app.get('/api/articles', (req, res) => {
     //res.status(200).json({ msg: 'working' }); this line to test message show in postman
@@ -106,5 +107,15 @@ app.get('/api/articles', (req, res) => {
         //respon to artical  to be as json
         res.status(200).json({ articles: articles });
     }).catch(e => console.log(e))
+    //http://localhost:3000/api/article/1
+
+    app.get('/api/article/:id', (req, res) => {
+        //  res.status(200).json({message: 'working'});
+        models.Article.findByPk(req.params.id).then(article => {
+            res.status(200).json({ article: article });
+        }).catch(e => console.log(e));
+    })
+
+
 })
 app.listen(port, () => console.log(`express - api app listening on port ${port} !`));
