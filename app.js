98056ca9f537f64290d686f2726c51d3ea41433d
Abdullah <ab4ullahkh@gmail.com>
2019-03-21 10:37:42 +0300

    Allow the user to update an existing person

--- app.js
+++ app.js
@@ -67,17 +67,24 @@ app.post('/api/person', (req,res) => {
 
 
 // update the person 
-// app.put('/api/person/:id', (req,res) => {
-//     models.Person.update({
-//         first_name: req.body,
-//         last_name: req.body
-//     })
-// },
-// {
-//   where: {
-//     id: req.params.id
-//   }
-// }).then( (result) => res.json(result) )
+app.put('/api/person/:id', (req, res) => {
+    // Find person By ID sent to us by user in the URL
+    models.Person.findByPk(req.params.id)
+    .then(person => {
+         // Call the Update function on the Person the database sent us back.
+        // Only update the fields I care about. 
+        person.update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        }).then(person => {
+       // The database was able to update the user
+      // And it sent us back an updated Record with the new information
+      // We can now send back this new information to the user
+            res.status(200).json({ person : person });
+        }).catch(e => console.log(e));
+    })
+    .catch(e => console.log(e));
+});
 
 
 // delete the person 
