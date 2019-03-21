33ba67d9406e88dafcd6cb021e38cdf37a3c35d1
Salem Alshehri <sal5m@hotmail.com>
2019-03-21 13:39:44 +0300

    Add route to show all articles

--- app.js
+++ app.js
@@ -108,5 +108,11 @@ app.delete('/api/person/:id', (req, res) => {
      .catch(e => console.log(e))
 })
 
+app.get('/api/articles', (req, res) => {
+models.Article.findAll().then(articles => {
+res.status(200).json({articles: articles});
+}).catch(e => console.log(e))
+})
+
 
 app.listen(port, () => console.log(`express-api and listening on port ${port}!`));
