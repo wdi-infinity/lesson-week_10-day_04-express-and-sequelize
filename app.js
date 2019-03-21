0b157c378d2270fc00bf98da150232646dc0266b
hessaAqeel <hessa1aqeel@gmail.com>
2019-03-21 16:16:13 +0300

    Add Relation between Articles and person and add route

--- app.js
+++ app.js
@@ -133,6 +133,7 @@ app.get('/api/articles', (req, res) => {
 })
 
 // get single article
+// localhost:3000/api/article/1
 app.get('/api/article/:id', (req, res) => {
     // res.status(200).json({ message: 'WORKING! ' });
     models.Article.findByPk(req.params.id).then(article => {
@@ -141,6 +142,31 @@ app.get('/api/article/:id', (req, res) => {
     }).catch(e => console.log(e));
 })
 
+// localhost:3000/api/person/1/articles
+// Get all articles by Person record ID 
+app.get('/api/person/:id/articles', (req, res) => {
+    // res.status(200).json({ message: 'WORKING! ' });
+    // res.status(200).json({ msg: req.params.id });
+    models.Person.findByPk(req.params.id, { include: [{ model: models.Article, }] }).then(person => {
+        // when calling one person by id --> an object of that perosn will show ; when using include: [{ model: Article }] --> the articles related to that peron appears
+        res.status(200).json({ person: person })
+    }).catch(e => console.log(e));
 
-app.listen(port, () => console.log(`express-api listening on port ${port}`))
+});
+
+
+// localhost:3000/api/article/1/comments
+
+
+// sync function / my web app should not run sync --> I dont want to listen to user until my app is synced to DB 
+models.sequelize.sync({}).then(() => {
+    console.log("SYNC COMPLETE ")
+    // models.Article.create({
+    //     title: "test2",
+    //     content: "this is a body2",
+    //     PersonId: 1
+    // });
+    app.listen(port, () => console.log(`express-api listening on port ${port}`))
+
+})
 
