639e1a184b426a5b3c93dbcf1b75f1c99bee66ad
asmaquraishi <asmaq66@gmail.com>
2019-03-21 11:58:56 +0300

    add articles sequlize model with seed data

---
+++ seeders/20190321081940-articles.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+
+       return queryInterface.bulkInsert('articles',[{
+         title: 'Hello',
+         content: 'this is content article.',
+         created_at: new Date,
+         updated_at: new Date
+
+       }],{});
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+
+   return queryInterface.bulkDelete('articles', null, {});
+
+  }
+};
