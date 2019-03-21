4a424af4373714d3bb9f0c9a846fbbe65bb4890b
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-21 10:51:05 +0300

    allow users to update person by ID

--- app.js
+++ app.js
@@ -85,6 +85,25 @@ app.delete('/api/person/:id', (req, res) => {
 
 });
 
+// Update an existing Person
+app.put('/api/person/:id', (req, res) => {
+    // Find Person By ID sent to us by User in the URL
+    models.Person.findByPk(req.params.id).then(person => {
+        // Call the Update function on the Person the database sent us back.
+        // Only update the fields I care about.
+        person.update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        }).then(person => {
+            // The database was able to update the user
+            // And it sent us back an updated Record with the new information
+            // We can now send back this new information to the user
+            res.status(200).json({ person: person });
+        }).catch(e => console.log(e));
+
+    }).catch(e => console.log(e));
+});
+
 const people = [
     { firstName: 'fajer', lastName: 'saleh' },
     { firstName: 'fajer', lastName: 'saleh' },
