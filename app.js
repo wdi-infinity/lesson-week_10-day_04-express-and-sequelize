6ebeaf153e02fa5044de9a93d6443cebcfba3992
sara saleh alofi <saraalawfi1995@gmail.com>
2019-03-21 11:14:26 +0300

    Update

--- app.js
+++ app.js
@@ -76,14 +76,38 @@ app.delete('/api/person/:id', (req, res) => {
         .catch(e => console.log(e));
 });
 
-app.put('/api/person/:id', (req, res) => {
-    models.Person.findByPk(req.body)
-        .then(personUpdateFromDb => {
-            res.status(200).json({ person: personUpdateFromDb });
-        })
-        .catch(e => console.log(e));
+//Update an existing Person
+// app.put('/api/person/:id', (req, res) => {
+//     models.Person.findByPk(req.body)
+//         .then(personUpdateFromDb => {
+//             res.status(200).json({ person: personUpdateFromDb });
+//         })
+//         .catch(e => console.log(e));
 
+// });
+
+
+
+// Update an existing Person
+http://localhost:3000/api/person/533
+app.put('/api/person/:id', (req, res) => {
+    // Find Person By ID sent to us by User in the URL
+    models.Person.findByPk(req.params.id).then(person => {
+        // Call the Update function on the Person the database sent us back.
+        // Only update the fields I care about.
+        person.update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        }).then(person => {
+            // The database was able to update the user
+            // And it sent us back an updated Record with the new information
+            // We can now send back this new information to the user
+            res.status(200).json({ person: person });
+        }).catch(e => console.log(e));
+
+    }).catch(e => console.log(e));
 });
+
 
 
 
