f2f566dd8171133e511cd026111ee8544377ea2a
loltisha <alyahadiem@gmail.com>
2019-03-21 16:55:40 +0300

    refactored people routes to new router file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import models from './models';
 import bodyParser from 'body-parser';
+import peopleRouter from './routes/peopleRoutes'
 const app = express();
 const port=3000;
 /*** Middleware ***/
 
 app.use(bodyParser.json());
+app.use(peopleRouter);
 // localhost: 3000/
 /** Routes path */
 app.get('/', (req, res) => {
@@ -23,73 +25,6 @@ app.get('/', (req, res) => {
 // ];
 
 
-//post http://localhost:3000/api/person/:id'  get person by record id 
-app.get('/api/person/:id', (req, res) => {
-  // res.status(200).json({user_id:'Working...'});
-  if(!isNaN(req.params.id)){
-    models.Person.findByPk(req.params.id)
-    .then(person => {
-      if (person!==null)
-      {  res.status(200).json({person: person});
-      }else{
-        res.status(404).json({error: 'person not found'});
-      }
-    })
-    .catch(e => console.log(e)); 
-  }else{
-    res.status(406).json({error: "invalid ID"});
-  }
-  
-  });
-// create new person
-  app.post('/api/person/', (req, res) => {
-    models.Person.create(req.body)
-    .then(personNewFromDB => {
-    res.status(201).json({person: personNewFromDB});
-    })
-    .catch(e => console.log(e))
-    
-  });
-  // delete existing person by record id
-  app.delete('/api/person/:id', (req, res) => {
-    models.Person.findByPk(req.params.id).then(person => {
-      person.destroy().then(() => {
-        res.status(200).json({
-          result: `Record ID ${req.params.id} Deleted`,
-        })
-      })
-      .catch(e => console.log(e)); 
-    })
-      .catch(e => console.log(e)); 
-  });
-  
-// localhost: 3000/api/people get all people
-app.get('/api/people', (req, res) => {
-  models.Person.findAll().then(people => {
-    res.status(200).json({
-      people: people,
-    });
-  })
-  .catch(e => console.log(e)); 
-});
-
-// update an existing person
-app.put('/api/person/:id', (req, res) => {
-  // find person by id sent to us by user in the url
-  models.Person.findByPk(req.params.id).then(person => {
-    // call the update function on the person the database sent us back 
-    // only update the fields i care about
-      person.update({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name
-      }).then(person =>{
-        // the database was able to update the user
-        // and it sent us back an updated record with the new information
-        // we can now sent back this new information to the user
-        res.status(200).json({person: person});
-      }).catch(e => console.log(e)); 
-    }).catch(e => console.log(e)); 
-});
 // localhost: 3000/api/articles get all articles
 app.get('/api/articles', (req, res) => {
   // res.status(200).json({message: 'working'});
@@ -105,30 +40,8 @@ app.get('/api/article/:id', (req, res) => {
     }).catch(e => console.log(e));
 })
 
-// localhost: 3000/api/person/1/articles
-// get all articles by person record id
-app.get('/api/person/:id/articles', (req, res) => {
-  models.Person.findByPk(req.params.id,{include: [{ model: models.Article }]}).then(person => {
-    res.status(200).json({person: person});
-    res.status(200).json({ msg: req.params.id });
-
-    }).catch(e => console.log(e));
-});
-
-
-
-
-
 
 // localhost: 3000/api/article/2/comments
-
-
-
-
-
-
-
-
 
 // eslint-disable-next-line no-console
 models.sequelize.sync().then(() => {
