256d601a7b38fbd102b49992133a90f05b9c06d0
sara saleh alofi <saraalawfi1995@gmail.com>
2019-03-21 13:40:49 +0300

    Add Route to show all Articles

--- app.js
+++ app.js
@@ -33,14 +33,20 @@ app.get('/api/person/:id', (req, res) => {
         models.Person.findByPk(req.body)
             .then(person => {
                 if (person !== null) {
-                    res.status(200).json({ person: person });
+                    res.status(200).json({
+                        person: person
+                    });
                 } else {
-                    res.status(404).json({ error: 'Person Not Found' });
+                    res.status(404).json({
+                        error: 'Person Not Found'
+                    });
                 }
             })
             .catch(e => console.log(e));
     } else {
-        res.status(406).json({ error: 'Invalid ID' });
+        res.status(406).json({
+            error: 'Invalid ID'
+        });
     }
 });
 
@@ -49,7 +55,9 @@ app.get('/api/person/:id', (req, res) => {
 app.post('/api/person', (req, res) => {
     models.Person.create(req.body)
         .then(personNewFromDb => {
-            res.status(201).json({ person: personNewFromDb });
+            res.status(201).json({
+                person: personNewFromDb
+            });
         })
         .catch(e => console.log(e));
 
@@ -89,24 +97,26 @@ app.delete('/api/person/:id', (req, res) => {
 
 
 // Update an existing Person
-http://localhost:3000/api/person/533
-app.put('/api/person/:id', (req, res) => {
-    // Find Person By ID sent to us by User in the URL
-    models.Person.findByPk(req.params.id).then(person => {
-        // Call the Update function on the Person the database sent us back.
-        // Only update the fields I care about.
-        person.update({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name
-        }).then(person => {
-            // The database was able to update the user
-            // And it sent us back an updated Record with the new information
-            // We can now send back this new information to the user
-            res.status(200).json({ person: person });
-        }).catch(e => console.log(e));
+http: //localhost:3000/api/person/533
+    app.put('/api/person/:id', (req, res) => {
+        // Find Person By ID sent to us by User in the URL
+        models.Person.findByPk(req.params.id).then(person => {
+            // Call the Update function on the Person the database sent us back.
+            // Only update the fields I care about.
+            person.update({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name
+            }).then(person => {
+                // The database was able to update the user
+                // And it sent us back an updated Record with the new information
+                // We can now send back this new information to the user
+                res.status(200).json({
+                    person: person
+                });
+            }).catch(e => console.log(e));
 
-    }).catch(e => console.log(e));
-});
+        }).catch(e => console.log(e));
+    });
 
 
 
@@ -115,7 +125,14 @@ app.put('/api/person/:id', (req, res) => {
 // localhost:3000/api/people
 // localhost:3000/api/person/2
 // POST localhost:3000/api/person/2
+//localhost:3000/api/articles
+
+app.get('/api/articles', (req, res) => {
+    models.Article.findAll().then(article => {
+        res.status(200).json({
+            articles: article
+        });
+    }).catch(e => console.log(e));
+})
 
 app.listen(port, () => console.log(`express-api app listening on port ${port}!`));
-
-
