418ee7d920b1757515383ae42f2e435ed60b6a58
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-21 11:58:55 +0300

    Add articales sequelize model with seed data

--- app.js
+++ app.js
@@ -71,16 +71,33 @@ app.put('/api/person/:id' , (req , res) => {
     const id = req.params.id
     models.Person.findByPk(id)
     .then( person => {
-        person.update({ id: req.body.id ,first_name: req.body.first_name , last_name: req.body.last_name
-
-                     }) 
-
+        person.update({ first_name: req.body.first_name , last_name: req.body.last_name }) 
         // person.update(req.body) 
         res.status(201).json({person: person})
     })
     .catch( e => console.log(e))
   
 })
+
+// // Update an existing Person
+// //http://localhost:3000/api/person/533
+// app.put('/api/person/:id', (req, res) => {
+//   // Find Person By ID sent to us by User in the URL
+//   models.Person.findByPk(req.params.id).then(person => {
+//     // Call the Update function on the Person the database sent us back.
+//     // Only update the fields I care about.
+//     person.update({
+//       first_name: req.body.first_name,
+//       last_name: req.body.last_name
+//     }).then(person => {
+//       // The database was able to update the user
+//       // And it sent us back an updated Record with the new information
+//       // We can now send back this new information to the user
+//       res.status(200).json({ person: person });
+//     }).catch(e => console.log(e));
+
+//   }).catch(e => console.log(e));
+// });
 
 //localhost:3000/api/person/:id to Delete person at a time
 
--- seeders/20190320110542-people.js
+++ seeders/20190320110542-people.js
@@ -7,12 +7,12 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:*/
-    //   return queryInterface.bulkInsert('people', [
-    //     {first_name: 'John Doe',last_name: 'Alsharhan', created_at: new Date, updated_at: new Date},
-    //     {first_name: 'Abdulmohsin',last_name: 'Alqhtani', created_at: new Date, updated_at: new Date},
-    //     {first_name: 'Dummy',last_name: 'Dummy', created_at: new Date, updated_at: new Date},
-    //     {first_name: 'Another name',last_name: 'last name', created_at: new Date, updated_at: new Date}
-    // ], {});
+      return queryInterface.bulkInsert('people', [
+        {first_name: 'John Doe',last_name: 'Alsharhan', created_at: new Date, updated_at: new Date},
+        {first_name: 'Abdulmohsin',last_name: 'Alqhtani', created_at: new Date, updated_at: new Date},
+        {first_name: 'Dummy',last_name: 'Dummy', created_at: new Date, updated_at: new Date},
+        {first_name: 'Another name',last_name: 'last name', created_at: new Date, updated_at: new Date}
+    ], {});
     
   },
 
