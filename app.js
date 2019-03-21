0aa7e2731bcb6602800c5fb71c007ede95a133b3
ceahmad <almansour.a.ahmad@gmail.com>
2019-03-21 11:10:11 +0300

    Done : update and delete

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ res.status(200).json({
 });
 
 
-// const people = [
-//     { firstName: 'Ahmad', lastName:'Almansor' },
-//     { firstName: 'Abdullah', lastName:'Ahmad' },
-//     { firstName: 'Abdullah', lastName:'Alfehaid' },
-//     { firstName: 'Ahmad', lastName:'Almansor' },
-//     { firstName: 'Ahmad', lastName:'Almansor' }    
-// ];
-
 
 app.get('/api/people', (req, res) => {
     models.Person.findAll()
@@ -71,8 +63,34 @@ res.status(201).json({ person: personNewFromDB });
 
 
 
-// localhost:3000/
-// localhost:3000/api/people
+
+app.put('/api/person/:id', (req, res) => {
+    models.Person.findByPk(req.params.id)
+     .then(person => {
+            person.update({ first_name: req.body.first_name, last_name: req.body.last_name}).then()(person => {
+                res.status(201).json({ person: person })
+                .catch(e => console.log(e)); 
+            })     
+     })
+     .catch(e => console.log(e)); 
+    });
 
 
-app.listen(3000, () => console.log(`express-api app listening on port ${port}`));
+
+
+
+    app.delete('/api/person/:id', (req, res) => {
+        const id = req.params.id
+        models.Person.findByPk(id)
+          .then(person => {
+            person.destroy().then(() => {
+              res.status(200).json({
+                result: `Record ID ${id} Deleted`,
+                success: true
+              });
+            })
+            .catch(e => console.log(e));
+          })
+          .catch(e => console.log(e));
+      });
+app.listen(port, () => console.log(`express-api app listening on port ${port}`));
