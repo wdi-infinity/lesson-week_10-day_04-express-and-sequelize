00422ffba71f1773d4beca802d616bdb6fc9660b
hala-almaimoni <halaalmaimoni@gmail.com>
2019-03-23 19:17:58 +0300

    completeing the homework 4

--- models/person.js
+++ models/person.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const Person = sequelize.define('Person', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING
-  }, {tableName: "people"});
-  Person.associate = function(models) {
+  }, { tableName: "people" });
+  Person.associate = function (models) {
     // associations can be defined here
   };
   return Person;
